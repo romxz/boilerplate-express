208e6826c51369af2fb196d53d8297c06d4c3859
andrei <andreiromalv@gmail.com>
2019-08-24 15:27:42 -0400

    completing express tutorial

--- myApp.js
+++ myApp.js
@@ -1,47 +1,73 @@
-
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
 // --> 7)  Mount the Logger middleware here
-
+app.use(function(req, res, next) {
+  console.log(req.method+' '+req.path+' - '+req.ip);
+  next();
+});
 
 // --> 11)  Mount the body-parser middleware  here
-
+app.use(bodyParser.urlencoded({extended: false}));
 
 /** 1) Meet the node console. */
-
+console.log("Hello World");
 
 /** 2) A first working Express Server */
+/*
+app.get('/', function(req, res) {
+  res.send('Hello Express');
+});*/
 
 
 /** 3) Serve an HTML file */
-
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
 
 /** 4) Serve static assets  */
-
+app.use(express.static(__dirname + '/public'));
 
 /** 5) serve JSON on a specific route */
-
+app.get('/json', function(req, res){
+  let retJSON;
+  if (process.env.MESSAGE_STYLE=='uppercase')  retJSON = 'HELLO JSON';
+  else retJSON = 'Hello json';
+  res.json({"message": retJSON});
+});
 
 /** 6) Use the .env file to configure the app */
- 
+// see.env and 5)
  
 /** 7) Root-level Middleware - A logger */
-//  place it before all the routes !
+// See above
 
 
 /** 8) Chaining middleware. A Time server */
-
+app.get('/now', function(req, res, next){
+  req.time = new Date().toString();
+  next();
+}, function(req, res) {
+  res.json({time: req.time});
+});
 
 /** 9)  Get input from client - Route parameters */
-
+app.get('/:word/echo', function(req, res) {
+  res.json({echo: req.params.word});
+});
 
 /** 10) Get input from client - Query parameters */
 // /name?first=<firstname>&last=<lastname>
-
+app.route('/name')
+  .get(function(req, res) {
+  res.json({name: req.query.first+' '+req.query.last});
+}).post(function(req, res){
+  res.json({name: req.body.first+' '+req.body.last});
+});
   
 /** 11) Get ready for POST Requests - the `body-parser` */
-// place it before all the routes !
+// See above
 
 
 /** 12) Get data form POST  */
@@ -54,4 +80,4 @@ var app = express();
 
 //---------- DO NOT EDIT BELOW THIS LINE --------------------
 
- module.exports = app;
+module.exports = app;
